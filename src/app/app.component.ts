0cac1ad6c9ec4c8599ee3fb0f68b2f285220a9df
Deyvid Marinho <demmarinho@gmail.com>
2019-03-05 17:20:52 -0400

    change primary class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var device;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    document.addEventListener("deviceready", () => alert(device.platform), false);
+  }
   title = 'testApp';
 }
